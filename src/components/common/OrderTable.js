9ce9b2cfecfe32d920cf4a99de9e37937e0e55fb
vivek malik <mohit@mohits-Air.Dlink>
2019-12-13 03:29:25 +0530

    change in order detail

--- src/components/common/OrderTable.js
+++ src/components/common/OrderTable.js
@@ -1,62 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import ConfirmDialog from './../../app/common/ConfirmDialog';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import NoDataAvailable from '../common/NoDataAvailable';
 
-
+import "../../assets/css/order.scss";
 const styles = theme => ({
-    heading: {
-        fontSize: '21px',
-        fontWeight: '500',
-        marginTop: '0',
-        marginBottom: '0',
-        fontFamily: 'Montserrat, sans-serif',
-    },
-    dialogPaper: {
-        minWidth: '80%',
-        // maxWidth: '700px',
-        minHeight: '700px',
-        // maxHeight: '500px'
-    },
-    tableCell: {
-        paddingLeft: '4px',
-        paddingRight: '4px',
-        textAlign: 'center',
-        maxWidth: '200px'
-    },
-    formAddBtn: {
-        width: '90%',
-        borderRadius: '10px',
-        fontSize: '20px',
-        textTransform: 'uppercase',
-        backgroundColor: '#4d9fa0 ',
-        color: '#fff',
-        height: '45px',
-        marginBottom: '15px',
-        marginTop: "11px",
-        marginRight: 'auto',
-        marginLeft: 'auto'
-    },
-    formRoot: {
-        // display: 'flex',
-        flexWrap: 'wrap',
-        width: '100%',
-        // marginLeft: '25%',
-        border: '1px solid #ccc',
-        boxShadow: '2px 2px 6px 0px  rgba(0,0,0,0.3)',
-        borderRadius: '4px',
-        marginBottom: '20px',
-        marginTop: '8%',
-        padding: '25px',
-        textAlign: 'center'
-    }
+    card: {
+       width:'100%',
+        margin: "auto",
+        transition: "0.3s",
+        boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+        "&:hover": {
+          boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+        }
+      },
+
 
 });
 
@@ -71,84 +30,40 @@ class OrderTable extends Component {
 
     }
     componentDidMount() {
-     
-
-    }
-
-
-  
-    getTableCellClass(classes, index) {
-        return classes.tableCell;
-    }
-
-    handleDialogCancel(event) {
-        this.props.onEditModalCancel();
-    }
-
-
-    handleAddClick(event) {
 
 
     }
+
+
     render() {
         const { classes } = this.props;
         return (<div style={{ width: '100%' }}>
-            <Table className='table-body'>
-                <TableHead>
-                    <TableRow  >
-                        {this.state.tableHeadData.map((option, i) => (
-                            <TableCell key={option} className={this.getTableCellClass(classes, i)} style={{ minWidth: '120px', paddingLeft: i === 0 ? '22px' : '' }}>{option}</TableCell>
-                        ))}
-                        {/* <TableCell key="star" className={this.getTableCellClass(classes, 4)} style={{ minWidth: '50px', color: "goldenrod", textAlign: 'left' }}> Quantity </TableCell> */}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {this.state.tableBodyData.length > 0 && this.state.tableBodyData.map((row, i) => {
-                        return (
-
-                            <TableRow key={'table_' + i} style={i % 2 === 0 ? { background: "#e5e8ec" } : { background: "#fff" }}>
-                                <TableCell component="th" scope="row" className={this.getTableCellClass(classes, 0)}>
-                                    {/* <Tooltip title={row.active ? "Enabled" : "Disabled"} placement="top" classes={{ tooltip: classes.lightTooltip }}> */}
-                                    {row.supplier_mobile}
-                                    {/* </Tooltip> */}
-                                </TableCell>
-                                <TableCell component="th" scope="row" className={this.getTableCellClass(classes, 0)}>
-                                    {row.buyer_mobile}
-
-                                </TableCell>
-                                {/* <TableCell className={this.getTableCellClass(classes, 2)}>
-                                    <Tooltip title={row.supplier_name ? row.supplier_name : ""} placement="top" classes={{ tooltip: classes.lightTooltip }}>
-                                        <div className="text-ellpses">{row.supplier_name}</div>
-                                    </Tooltip>
-
-                                </TableCell>
-                                <TableCell className={this.getTableCellClass(classes, 3)}>
-                                    <Tooltip title={row.buyer_name ? row.buyer_name : ""} placement="top" classes={{ tooltip: classes.lightTooltip }}>
-                                        <div className="text-ellpses">{row.buyer_name}</div>
-                                    </Tooltip>
-                                </TableCell>
-
-                                <TableCell className={this.getTableCellClass(classes, 4)}>
-                                    {row.broker_name}
-                                </TableCell> */}
-                                <TableCell className={this.getTableCellClass(classes, 5)} >
-                                    {row.bijak_amt}
-                                </TableCell>
-                                <TableCell className={this.getTableCellClass(classes, 6)} >{row.commodity}</TableCell>
-                                <TableCell className={this.getTableCellClass(classes, 7)} >{row.createdtime.split("T")[0]}
-                                </TableCell>
+            <div class="mdl-card mdl-shadow--2dp mdl-card--horizontal">
+                <div class="mdl-card__media">
+                    <img src="http://placehold.it/150x200/DC143C/FFFFFF" alt="img" />
+                </div>
+                <div class="mdl-card__title">
+                    <h2 class="mdl-card__title-text">Welcome</h2>
+                </div>
+                <div class="mdl-card__supporting-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris sagittis pellentesque lacus eleifend lacinia...
+    </div>
+                <div class="mdl-card__actions mdl-card--border">
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" data-upgraded=",MaterialButton,MaterialRipple">Get Started</a>
+                </div>
+                <div class="mdl-card__menu">
+                    <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" data-upgraded=",MaterialButton,MaterialRipple"><i class="material-icons">share</i></button>
+                </div>
+            </div>
 
 
-                            </TableRow>
-                        );
-                    })}
-                </TableBody>
 
-            </Table>
-            {!this.state.tableBodyData.length && < NoDataAvailable style={{ height: '25vh' }} />}
+            \
+                    {!this.state.tableBodyData.length && < NoDataAvailable style={{ height: '25vh' }} />}
 
             {/* <Button className={classes.formCancelBtn} onClick={this.handleAddClick.bind(this)} color="primary">Sumbit</Button> */}
-            <Button style={{float:'right',marginRight:'28px'}} onClick={this.handleDialogCancel.bind(this)} color="primary">Cancel</Button>
+            {/* <Button style={{float:'right',marginRight:'28px'}} onClick={this.handleDialogCancel.bind(this)} color="primary">Cancel</Button> */}
 
             {this.state.showConfirmDialog ?
                 <ConfirmDialog
