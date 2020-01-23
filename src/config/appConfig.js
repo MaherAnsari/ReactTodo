5f8c14fd0dcdf4e21e0e8a2faf5530fa708072c6
binod <Binod>
2020-01-23 23:53:07 +0530

    changes of role

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,94 +1,43 @@
 export function getAccessAccordingToRole(appRoute, option) {
     try {
         let role = sessionStorage.getItem("userRole") || "dev";
-        let devAccess = {
-
-            //    Supporting Data
-            "mandi-data": {
-                "showTab": true,
-                "addLocation": true
-            },
-            "mandi-rates": {
-                "showTab": true,
-                "addMandiRates": true
-            },
-            "comodity-list": {
-                "showTab": true,
-                "updateACtiveStatus": true,
-                "editCommodityData": true
-            },
-
-            //    Bussiness Data 
-            "rate-list": {
-                "showTab": true,
-                "addPrice": true,
-            },
-            "orders-list": {
-                "showTab": true,
-                "payViaCredit": true,
-                "editOrder": true,
-                "addOrder": true,
-                "uploadFile": true
-            },
-            "payment": {
-                "showTab": true,
-                "addTransaction": true,
-                "uploadFile": true,
-                "editTransaction": true,
-                "updateStatus": true
-            },
-            "todays-payment": {
-                "showTab": true,
-                "updatePayout": true,
-                "editPayment": false,
-                "editUser": false,
-                "updateCredit": false,
-                "addNewAccount":false
-            },
-            "add-bank-account":{
-                "showTab": true,
-                "addNewAccount": false
-
-            },
-            // User Data
-            "user-list": {
-                "showTab": true,
-                "uploadFile": true,
+        let roles = {
+            "user-creation": {
                 "addUser": true,
-                "addTransaction": true,
+                "editUser": true
+            },
+            "add-bank":{
+                "addBankAccount" : true
+            },
+ 
+            "order-creation":{
                 "addOrder": true,
-                "editUser": true,
+                "editOrder": true,
+            },
+            "payviaCreditption":{
+                "payViaCredit": true
+            },
+ 
+            "payment-request":{
+                "addBank": true,
+                "payViaCredit": true,
+            },
+            "manage-credit":{
                 "updateCreditLimit": true
             },
-            "broker-list": {
-                "showTab": true,
-                "addBroker": true,
-                "uploadFile": true,
-                "addOrder": true,
-                "addTransaction": true,
-                "editUser": true,
-                "updateCreditLimit": true
+            "manange-supporting-data":{
+                "addCommodity": true,
+                "addMandi": true,
+                "addSupportingImages": true
             },
-            "buyer-list": {
-                "showTab": true,
-                "addBuyer": true,
-                "uploadFile": true,
-                "addOrder": true,
-                "addTransaction": true,
-                "editUser": true,
-                "updateCreditLimit": true
-            },
-            "supplier-list": {
-                "showTab": true,
-                "addSupplier": true,
-                "uploadFile": true,
-                "addTransaction": true,
-                "addOrder": true,
-                "editUser": true,
-                "updateCreditLimit": true
-            }
-        }
-
+            "payment-tab-entry-rule":{
+             "addPayment":true,
+             "editPayment":true
+             },
+             "super-admin":{
+                 "allowAll": true
+             }
+         }
         if (role === "dev") {
             return devAccess[appRoute][option];
         } else {
