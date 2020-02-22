d5224a7c29d4e5674c6db99b0497c9530e91f2a9
binod <Binod>
2020-02-23 03:27:29 +0530

    chnages of BUTTON FOOTER position to bottom

--- src/components/common/UserDetailsFooter.js
+++ src/components/common/UserDetailsFooter.js
@@ -50,7 +50,7 @@ class UserDetailsFooter extends Component {
     render() {
         const { classes } = this.props;
         return (
-            <div style={{ marginTop: "10px",marginBottom:'10px'}}>
+            <div style={{ marginTop: "10px",marginBottom:'10px', bottom: "25px" ,position: "absolute"}}>
                 { !this.state.isPaymentInfoModal &&  getAccessAccordingToRole("addOrder") && 
                 <span className={classes.span_main}
                 onClick={( event )=> this.onButtonClicked( "orders" )}
